a5961c95dbdbf344703898650baf77ffe6a71085
RubyEnoshima <vilaruben70@gmail.com>
2021-12-23 18:25:48 +0100

    Placeholder de gameover

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public void Revivir(){
+        Debug.Log("Revivo");
+    }
+
+    public void Quit(){
+        Debug.Log("Quiteo");
+    }
+}
