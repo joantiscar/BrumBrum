fc6a4d17f8c0dfb0bfd5d6e847f315d983670cf8
DanielVazSam <u1967096@campus.udg.edu>
2022-01-23 21:04:12 +0100

    combate aleatorio

--- Assets/Scripts/SceneDialoguesScripts/Scene3_Castle_Start.cs
+++ Assets/Scripts/SceneDialoguesScripts/Scene3_Castle_Start.cs
@@ -31,6 +31,7 @@ public class Scene3_Castle_Start : MonoBehaviour
         else if (!FindObjectOfType<controlDialegs>().animSeguit.GetBool("Seguit") /*&& !secondDialogueIsCalled*/)
         {
             GameObject.Find("Scenario_ThirdScene").GetComponent<AudioSource>().volume = 1;
+            GameObject.Find("Scenario_ThirdScene").GetComponent<RandomCombat>().SetAble();
             Destroy(npc_inicialDialogue.transform.parent.gameObject);
         }
     }
