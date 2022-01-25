57a0d9e0df22221a9be5d24962ec9645fabed1e4
RubyEnoshima <vilaruben70@gmail.com>
2022-01-26 00:02:24 +0100

    Comienzo cambios ui y otras cosas que no recuerdo

--- Assets/Scripts/CamaraFundido.cs
+++ Assets/Scripts/CamaraFundido.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CamaraFundido : MonoBehaviour
 {
-    Canvas canvas;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        // canvas = this.transform.GetChild(0);
+        
     }
 
     // Update is called once per frame
